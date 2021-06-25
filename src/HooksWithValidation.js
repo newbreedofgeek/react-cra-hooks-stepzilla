a49fdd0f3079724259e5e39fa2fc7304a5737a1a
Mark Paul <paul.mark@gmail.com>
2021-06-26 00:08:13 +1000

    upgrade react version to latest, convert sandbox stepzilla to react hooks. all works as when it was a class component

--- src/HooksWithValidation.js
+++ src/HooksWithValidation.js
@@ -1,15 +1,15 @@
 import React, {useState} from 'react';
 
-export default function HooksWithValidation({jumpToStep}) {
+const HooksWithValidation = ({jumpToStep}) => {
   const [valid, setValid] = useState(false);
   
   const toggleValidState = () => {
     setValid(!valid);
   }
 
-  const isValidated = () => {
-    return valid;
-  }
+  // const isValidated = () => {
+  //   return valid;
+  // }
 
   return <>
     I am a Hooks based Functional Component: My validation state is {valid.toString().toUpperCase()}
@@ -18,3 +18,9 @@ export default function HooksWithValidation({jumpToStep}) {
     <div className="toggle" onClick={() => jumpToStep(2)}>[Jump to Step 2]</div>
   </>
 }
+
+HooksWithValidation.isValidated = () => {
+  return true;
+}
+
+export default HooksWithValidation;
